5c35b1507f860488e6bd1d6b9f3a8fe2591a6200
Jay Zeschin <jay@zeschin.org>
2011-08-15 00:49:59 -0600

    Cache this puppy

--- Gemfile
+++ Gemfile
@@ -1,11 +1,13 @@
 source :rubygems
 
 gem 'sinatra',              '1.2.6'
-gem 'sinatra-reloader',   '0.5.0'
+gem 'sinatra-reloader',     '0.5.0'
 gem 'haml',                 '3.1.1'
 gem 'sass',                 '3.1.1'
 gem 'maruku'
 gem 'thin'
+gem 'dalli'
+gem 'rack-cache'
 
 group :development do
   gem 'heroku',                       :require => false
--- app.rb
+++ app.rb
@@ -3,6 +3,8 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'haml'
 require 'sass'
+require 'dalli'
+require 'rack/cache'
 
 module App
   class Application < Sinatra::Base
@@ -12,6 +14,21 @@ module App
 
     configure :development do
       register Sinatra::Reloader
+    end
+
+    configure :staging, :production do
+
+      set :cache, Dalli::Client.new
+
+      use Rack::Cache,
+        :verbose      => true,
+        :metastore    => settings.cache,
+        :entitystore  => settings.cache
+
+      before do
+        cache_control :public, :max_age => 2 * 60
+      end
+      
     end
 
     get '/stylesheets/:name.css' do
