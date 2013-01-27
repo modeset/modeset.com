b3687754bfa292a094ded4132bbef951444e20a8
Jay Zeschin <jay.zeschin@modeset.com>
2013-01-26 21:27:31 -0700

    Process the summary with HTML::Pipeline too

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,27 @@
 module ApplicationHelper
+
   def sections
     %w(share-the-love who-we-are index how-we-work)
   end
 
+
+  # Our own versions of the content and summary helpers
   def process_content_with_pipeline(post)
     HTML::Pipeline.new([
       HTML::Pipeline::MarkdownFilter,
       HTML::Pipeline::SyntaxHighlightFilter
     ]).call(post.content)[:output].to_s.html_safe
   end
+
+  def post_summary(post)
+    if post.summary.present?
+      content_tag :p, post.summary
+    else
+      html = Sanitizer.new.sanitize(process_content_with_pipeline(post))
+      doc = Nokogiri::HTML.fragment(html)
+      para = doc.search('p').detect { |p| p.text.present? }
+      para.try(:to_html).try(:html_safe)
+    end
+  end
+
 end
